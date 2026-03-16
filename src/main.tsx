ae02c9eba0e23918386df007d1221db02313d459
Devin Menard <menard.devin@gmail.com>
2026-03-16 09:40:28 -0400

    Deployment: Version 1.0.5 - Fresh commit to bypass redeploy block

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-console.log('MAIN.TSX: Initialization started - Version 1.0.4');
+console.log('MAIN.TSX: Initialization started - Version 1.0.5');
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
