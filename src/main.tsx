1b91f1b0e6d7796058283226b1abe93c7184d9f7
Devin Menard <menard.devin@gmail.com>
2026-03-15 22:17:16 -0400

    Deployment: Version 1.0.2 - Force redeploy with naming fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-console.log('MAIN.TSX: Initialization started - Version 1.0.1');
+console.log('MAIN.TSX: Initialization started - Version 1.0.2');
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
